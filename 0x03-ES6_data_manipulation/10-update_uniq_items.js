8eb0cccb633f55ff7bf15e6245ffec924c3507c8
bdftyousra@gmail.com <bdftyousra@gmail.com>
2023-12-04 23:20:59 -0800

    all file test

---
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -0,0 +1,10 @@
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+  map.forEach((value, key) => {
+    if (value === 1) {
+      map.set(key, 100);
+    }
+  });
+}
